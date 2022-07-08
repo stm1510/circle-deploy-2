d2784a86f02ecf88121d2845e94a36b82024f62b
stm1510 <tawfiqsani54@gmail.com>
2022-07-09 01:29:16 +0200

    modification 17

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()// here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth() // here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
@@ -47,6 +47,7 @@ async function bootstrap() {
   };
   app.use(cors(corsOptions));
   app.useGlobalFilters(new ErrorFilter());
+  console.log("App about to listen!")
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
 }
